26ef51b49e757f8c70ff9dd3172299139fd95e4b
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-03-17 10:33:52 +0100

    save 10h35

--- moviesandme/Helpers/filmsData.js
+++ moviesandme/Helpers/filmsData.js
@@ -15,7 +15,7 @@ let films = [
     id: 181809,
     vote_average: 8.1,
     title: 'La Guerre des étoiles',
-    poster_path: '',
+    poster_path: require ('../assets/filmVide.png'),
     original_title: 'Star Wars',
     overview:
       "Il y a bien longtemps, dans une galaxie très lointaine... La guerre civile fait rage entre l'Empire galactique et l'Alliance rebelle. Capturée par les troupes de choc de l'Empereur menées par le sombre et impitoyable Dark Vador, la princesse Leia Organa dissimule les plans de l’Étoile Noire, une station spatiale invulnérable, à son droïde R2-D2 avec pour mission de les remettre au Jedi Obi-Wan Kenobi. Accompagné de son fidèle compagnon, le droïde de protocole C-3PO, R2-D2 s'échoue sur la planète Tatooine et termine sa quête chez le jeune Luke Skywalker. Rêvant de devenir pilote mais confiné aux travaux de la ferme, ce dernier se lance à la recherche de ce mystérieux Obi-Wan Kenobi, devenu ermite au cœur des montagnes désertiques de Tatooine...",
