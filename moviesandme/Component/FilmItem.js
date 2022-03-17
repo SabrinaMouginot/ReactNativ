da60864cddca0fa9fbea23177f1d88689b776270
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-03-17 09:26:36 +0100

    save 9h30

--- moviesandme/Component/FilmItem.js
+++ moviesandme/Component/FilmItem.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
+import JSONPretty from 'react-json-pretty'
 
 class FilmItem extends React.Component{
     render() {
         const film = this.props.film
         console.log(this.props.film.title)
         return (
+        <View>
             <View>
+                <JSONPretty data={film}></JSONPretty>
+            </View>
+            <View style={styles.film_main_container}>
                 <Text>{film.title}</Text>
                 <Image
                     style={styles.image}
@@ -14,14 +19,18 @@ class FilmItem extends React.Component{
                         uri: 'https://reactnative.dev/img/tiny_logo.png',
                     }}
                 />
-            </View>)
+                </View>
+        </View>)
     }
 }
 
 const styles = StyleSheet.create({
+    film_main_container: {
+        flexDirection: 'row',
+    },
     image: {
-        width: 20,
-        height: 20,
+        width: 120,
+        height: 180,
     },
     })
 export default FilmItem
