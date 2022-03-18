268112609494212339e3a6130e75adaaaabb74ec
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-03-18 10:48:54 +0100

    save 10h48

--- moviesandme/Component/Search.js
+++ moviesandme/Component/Search.js
@@ -1,9 +1,9 @@
 // Components/Search.js
-import React from 'react'
-import { FlatList, View, TextInput, Button, StyleSheet } from 'react-native'
-import films from '../Helpers/filmsData'
-import FilmItem from './FilmItem'
-import getFilmsFromApiWithSearchedText from '../API/TMDBApi'
+import React from "react";
+import { FlatList, View, TextInput, Button, StyleSheet, SafeAreaView } from "react-native";
+import films from "../Helpers/filmsData";
+import FilmItem from "./FilmItem";
+import getFilmsFromApiWithSearchedText from "../API/TMDBApi";
 
 
 const styles = StyleSheet.create({
@@ -15,59 +15,79 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginRight: 5,
     height: 50,
-    borderColor: '#346',
+    borderColor: "#346",
     borderWidth: 1,
     paddingLeft: 5,
   },
   loading_container: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
     top: 100,
     bottom: 0,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-})
+});
 
 class Search extends React.Component {
-	// ajouter la prop films, une liste vide au départ
-	constructor(props) {
-		super(props);
-    this.state = { films: [] };
-	}
-	_loadFilms() {
-        getFilmsFromApiWithSearchedText('star').then((data) => {
-			// appel de setSate, actualistion automatique de la Flatlist
-			this.setState({ films: data.results });
-			// enlever le forceUpdate()
+  // ajouter la prop films, une liste vide au départ
+  constructor(props) {
+    super(props);
+	this.state = { films: [], height: 0 };
+  }
+  _loadFilms() {
+    getFilmsFromApiWithSearchedText("star").then((data) => {
+      // appel de setSate, actualistion automatique de la Flatlist
+      this.setState({ films: data.results });
+      // enlever le forceUpdate()
 
-			console.log(
-				'--_loadFilms\n' +
-					JSON.stringify(data) +
-					'\n_loadFilms--' +
-					data.results[0].original_title +
-					'\n--_loadFilms--'
-			);
-		});
+      console.log(
+        "--_loadFilms\n" +
+          JSON.stringify(data) +
+          "\n_loadFilms--" +
+          data.results[0].original_title +
+          "\n--_loadFilms--"
+      );
+    });
 	}
-	render() {
-		return (
-			<View style={styles.main_container}>
-				<TextInput style={styles.textinput} placeholder="Titre du film" />
+	
 
-				<Button title="Rechercher" onPress={() => this._loadFilms()} />
-				<View>
-					<FlatList
-						// les données de la FlatList doivent être actualisées à chaque appel de setState
-						data={this.state.films}
-						keyExtractor={(item) => item.id.toString()}
-						renderItem={({ item }) => <FilmItem film={item} />}
-					/>
-				</View>
-			</View>
-		);
-	}
+
+  render() {
+    return (
+      <SafeAreaView style={styles.main_container}>
+        <TextInput style={styles.textinput} placeholder="Titre du film" />
+
+        <View>
+          <Button title="Rechercher" onPress={() => this._loadFilms()} />
+
+          <View style={{ flex: 1 }} contentContainerStyle={{ flex: 1 }}>
+            <FlatList
+              onLayout={(e) => {
+                this.setState({ height: e.nativeEvent.layout.height }); //setstate pr ne pas charger les données à l'infini
+                console.log(e.nativeEvent.layout.height);
+              }}
+              style={{
+                flexGrow: 1,
+                height: this.state.height,
+              }}
+              onEndReachedThreshold={0.5}
+              onEndReached={() => {
+                if (this.page < this.totalPages) {
+                  // On vérifie qu'on n'a pas atteint la fin de la pagination (totalPages) avant de charger plus d'éléments
+                  this._loadFilms();
+                }
+              }}
+              data={this.state.films}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => <FilmItem film={item} />}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
-export default Search
+export default Search;
